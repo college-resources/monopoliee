d3826d7693b87899f596d3e22e6c506e3afafd99
jim3692 <jim3692@gmail.com>
2019-11-23 18:36:37 +0200

    Added login form and Socket.io WebGL template

---
+++ Unity/Assets/Scripts/Login.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+    public TextInput username;
+    public TextInput password;
+
+    public void onClick()
+    {
+        Debug.Log(username.Value);
+        Debug.Log(password.Value);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity/Assets/Scripts/TextInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class TextInput : MonoBehaviour
+{
+    private InputField _inputField;
+    
+    public string Value
+    {
+        get => _inputField.text;
+        set => _inputField.text = value;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _inputField = GetComponentInChildren<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
