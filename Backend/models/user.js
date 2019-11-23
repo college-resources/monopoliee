1b1ee8884c2abbe6bef205bf8988e3559131effb
jim3692 <jim3692@gmail.com>
2019-11-23 13:49:20 +0200

    Implemented authentication

---
+++ Backend/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    passwordHash: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('User', userSchema)
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,5 +1,8 @@
+const bcrypt = require('bcrypt')
 const express = require('express')
 const { check, validationResult } = require('express-validator')
+
+const User = require('../models/user')
 
 const router = express.Router()
 
@@ -8,19 +11,77 @@ module.exports = router
 router.post('/login', [
   check('username').isLength({ min: 4 }),
   check('password').isLength({ min: 6 })
-], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
+], async (req, res, next) => {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+
+    if (req.session.user) {
+      return res.status(400).json({ error: { message: 'Already logged in' } })
+    }
+
+    const user = await User.findOne({ username: req.body.username })
+    if (user) {
+      const match = bcrypt.compareSync(req.body.password, user.passwordHash)
+      delete req.body.password
+      if (match) {
+        req.session.user = {
+          _id: user.id,
+          username: user.username
+        }
+        return res.json({ message: 'Logged in successfully' })
+      }
+    }
+
+    return res.status(401).json({ error: { message: 'Invalid Credentials' } })
+  } catch (err) {
+    next(err)
   }
+})
+
+router.get('/logout', (req, res) => {
+  if (req.session.user) {
+    delete req.session.user
+    return res.json({ message: 'Logged out successfully' })
+  }
+
+  return res.status(403).json({ error: { message: 'Not logged in' } })
 })
 
 router.post('/register', [
   check('username').isLength({ min: 4 }),
   check('password').isLength({ min: 6 })
-], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
+], async (req, res, next) => {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+
+    if (req.session.user) {
+      return res.status(400).json({ error: { message: 'Already logged in' } })
+    }
+
+    const user = await User.findOne({ username: req.body.username })
+    if (!user) {
+      const hash = bcrypt.hashSync(req.body.password, 10)
+      delete req.body.password
+
+      const user = new User({
+        username: req.body.username,
+        passwordHash: hash
+      })
+      await user.save()
+
+      req.session.user = { username: user.username }
+
+      return res.json({ message: 'User registered successfully' })
+    }
+
+    return res.status(400).json({ error: { message: 'Username already exists' } })
+  } catch (err) {
+    next(err)
   }
 })
