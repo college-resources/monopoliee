b762483c9e482d49ccffb9bb7c4c3af04d3a83e3
jim3692 <jim3692@gmail.com>
2019-11-24 13:11:23 +0200

    Connected Unity login form to authentication backend

--- Unity/Assets/Scripts/Login.cs
+++ Unity/Assets/Scripts/Login.cs
@@ -1,27 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Login : MonoBehaviour
 {
     public TextInput username;
     public TextInput password;
 
-    public void onClick()
+    public void OnLogin()
     {
         Debug.Log(username.Value);
         Debug.Log(password.Value);
+        
+        StartCoroutine(Upload(ApiAction.Login));
+    }
+
+    public void OnLogout()
+    {
+        StartCoroutine(Upload(ApiAction.Logout));
     }
     
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator Upload(ApiAction action)
     {
+        switch (action)
+        {
+            case ApiAction.Login:
+                WWWForm form = new WWWForm();
+                form.AddField("username", username.Value);
+                form.AddField("password", password.Value);
+
+                using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:3000/auth/login", form))
+                {
+                    yield return www.SendWebRequest();
+
+                    if (www.isNetworkError || www.isHttpError)
+                    {
+                        Debug.Log(www.error);
+                    }
+                    else
+                    {
+                        Debug.Log("Form upload complete!");
+                    }
+            
+                    Debug.Log(www.downloadHandler.text);
+                }
+                break;
+            case ApiAction.Logout:
+                using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:3000/auth/logout"))
+                {
+                    yield return www.SendWebRequest();
+
+                    if (www.isNetworkError || www.isHttpError)
+                    {
+                        Debug.Log(www.error);
+                    }
+                    else
+                    {
+                        Debug.Log("Form upload complete!");
+                    }
+            
+                    Debug.Log(www.downloadHandler.text);
+                }
+                break;
+            default:
+                break;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    enum ApiAction
     {
-        
+        Login, Logout
     }
 }
