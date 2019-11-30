bed53f2f45f05fa4ba0cef2196a485b4563ba02e
Stefanos Papanatsios <spapanatsios@gmail.com>
2019-11-30 14:00:22 +0200

    Added Board, NavMeshAgent and Movement script for testing

---
+++ Unity/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public Camera camera;
+    public NavMeshAgent agent;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
