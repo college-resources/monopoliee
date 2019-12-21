291e4ef5562dec8b900e6362465aac02e490ad62
jim3692 <jim3692@gmail.com>
2019-12-21 20:42:19 +0200

    Added New Game dialog to Lobby

---
+++ Unity/Assets/Scripts/Lobby/NewGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewGame : MonoBehaviour
+{
+    public GameObject dialog;
+    private Button _button;
+    
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(delegate
+        {
+            dialog.SetActive(true);
+        });
+    }
+}
---
+++ Unity/Assets/Scripts/Lobby/NewGameDialog.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewGameDialog : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject create;
+    public GameObject seats;
+
+    private void Awake()
+    {
+        Button canvasBtn = canvas.GetComponent<Button>();
+        canvasBtn.onClick.AddListener(() =>
+        {
+            gameObject.SetActive(false);
+        });
+
+        TextInput seatsTxt = seats.GetComponent<TextInput>();
+        Button createBtn = create.GetComponent<Button>();
+        createBtn.onClick.AddListener(() =>
+        {
+            seatsTxt.Highlight = false;
+            
+            int seatsNumber;
+            if (int.TryParse(seatsTxt.Value, out seatsNumber) && seatsNumber > 0)
+            {
+                APIWrapper.Instance.GameNew(seatsNumber, (response, error) =>
+                {
+                    if (error != null)
+                    {
+                        if (response["error"] != null)
+                        {
+                            Debug.Log((string) response["error"]["message"]);
+                        }
+                        else if (response["errors"] != null)
+                        {
+                            JArray errors = (JArray) response["errors"];
+                            foreach (JToken err in errors.Children())
+                            {
+                                if ((string) err["msg"] == "Invalid value")
+                                {
+                                    if ((string) err["param"] == "seats")
+                                    {
+                                        seatsTxt.Highlight = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+            else
+            {
+                seatsTxt.Highlight = true;
+            }
+        });
+    }
+}
