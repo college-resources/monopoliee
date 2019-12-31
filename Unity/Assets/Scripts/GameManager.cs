e0ecfd91aac690cf74740e4ea599dc073a1bcd73
jim3692 <jim3692@gmail.com>
2019-12-31 18:24:18 +0200

    Uploaded GameManager.cs

---
+++ Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Schema;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    
+    public static GameManager Instance { get => _instance; }
+
+    private static GameManager _instance;
+    
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    
+    #endregion
+
+    private Game _game;
+
+    public void GoToGame(Game game)
+    {
+        _game = game;
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
