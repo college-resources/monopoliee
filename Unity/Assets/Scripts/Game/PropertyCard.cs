874d2629da053dfc76dfc4f196bc22b815ff8ec2
CrazyStevenz <spapanatsios@gmail.com>
2020-01-04 23:11:07 +0200

    Added PropertyCard ScriptableObject and PropertyCardDisplay script

---
+++ Unity/Assets/Scripts/Game/PropertyCard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Property")]
+public class PropertyCard : ScriptableObject
+{
+    public new string name;
+    public int location;
+    public int price;
+    public int[] rents;
+    public int mortgage;
+    
+    public Sprite artwork;
+}
