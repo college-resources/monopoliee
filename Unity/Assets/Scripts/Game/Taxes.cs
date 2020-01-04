1c10d0d94f54bf9ba424336db1282968642b6b0f
feedmycat <konstantangelou@gmail.com>
2020-01-04 18:36:14 +0200

    Display the prices of properties and taxes from the database on the board

---
+++ Unity/Assets/Scripts/Game/Taxes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taxes : MonoBehaviour
+{
+    private TextMesh[] prices;
+    public List<TextMesh> pricesList = new List<TextMesh>();
+
+    private void Start()
+    {
+        pricesList.Clear();
+
+        prices = GetComponentsInChildren<TextMesh>();
+        foreach (TextMesh textMesh in prices)
+        {
+            pricesList.Add(textMesh);
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Lobby/PriceLoader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+public class PriceLoader : MonoBehaviour
+{
+    public List<int> propertiesPrices;
+    public List<int> taxesPrices;
+    public JArray prices;
+    public Properties properties;
+    public Taxes taxes;
+
+    private void Start()
+    {
+        APIWrapper.Instance.GamePrices((response, error) =>
+        {
+            if (error == null)
+            {
+                prices = (JArray) response["properties"];
+                propertiesPrices = new List<int>(prices.Count);
+                foreach (var price in prices)
+                {
+                    int p = (int) price["price"];
+                    propertiesPrices.Add(p);
+                    Debug.Log(p);
+                }
+                prices = (JArray) response["taxes"];
+                taxesPrices = new List<int>(prices.Count);
+                foreach (var price in prices)
+                {
+                    int p = (int) price["price"];
+                    taxesPrices.Add(p);
+                    Debug.Log(p);
+                }
+                LoadPrices();
+            }
+            else
+            {
+                throw new Exception(error);
+            }
+        });
+    }
+
+   public void LoadPrices()
+    {
+        for (int i = 0; i < propertiesPrices.Count; i++)
+        {
+            properties.pricesList[i].text = propertiesPrices[i] + "ΔΜ";
+        }
+        for (int i = 0; i < taxesPrices.Count; i++)
+        {
+            taxes.pricesList[i].text = taxesPrices[i] + "ΔΜ";
+        }
+    }
+}
