618d0bf6185cad4c481566e40c31e9ccc6a6ac79
feedmycat <konstantangelou@gmail.com>
2020-01-05 18:48:12 +0200

    Changed property card from scriptable object to normal class

--- Unity/Assets/Scripts/Game/PropertyCard.cs
+++ Unity/Assets/Scripts/Game/PropertyCard.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Property")]
-public class PropertyCard : ScriptableObject
+public class PropertyCard
 {
     public new string name;
     public string color;
     public int location;
-    public int price;
     public int[] rents;
+    public int houseCost;
     public int mortgage;
-    
-    public Sprite artwork;
+
+    public PropertyCard(string name, string color, int location, int[] rents, int houseCost, int mortgage)
+    {
+        this.name = name;
+        this.color = color;
+        this.location = location;
+        this.rents = rents;
+        this.houseCost = houseCost;
+        this.mortgage = mortgage;
+    }
 }
