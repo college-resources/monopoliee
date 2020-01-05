83d0819d53f3a8e081b5265d226e8572412f4ad7
CrazyStevenz <spapanatsios@gmail.com>
2020-01-05 20:22:31 +0200

    Added StationCard class

---
+++ Unity/Assets/Scripts/Game/StationCard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationCard
+{
+    public new string name;
+    public int location;
+    public int[] rents;
+    public int mortgage;
+
+    public StationCard(string name, int location, int[] rents, int mortgage)
+    {
+        this.name = name;
+        this.location = location;
+        this.rents = rents;
+        this.mortgage = mortgage;
+    }
+}
