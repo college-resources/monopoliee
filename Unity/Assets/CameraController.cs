2d45184ffc63869541c893973519007941812033
feedmycat <konstantangelou@gmail.com>
2020-01-06 02:26:35 +0200

    Added camera controller

---
+++ Unity/Assets/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public List<GameObject> cameraList;
+    private int currentCamera;
+    void Start () {
+        currentCamera = 0;
+        for (int i = 0; i < cameraList.Count; i++){
+            cameraList[i].gameObject.SetActive(false);
+        }
+
+        if (cameraList.Count > 0){
+            cameraList[0].gameObject.SetActive (true);
+        }
+    }
+ 
+    void Update () {
+        if (Input.GetMouseButtonUp(0)){
+            currentCamera ++;
+            if (currentCamera < cameraList.Count){
+                cameraList[currentCamera - 1].gameObject.SetActive(false);
+                cameraList[currentCamera].gameObject.SetActive(true);
+            }
+            else {
+                cameraList[currentCamera - 1].gameObject.SetActive(false);
+                currentCamera = 0;
+                cameraList[currentCamera].gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Unity/Assets/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CameraController cameraController;
+    void Start()
+    {
+        cameraController = GameObject.FindWithTag("GameController").GetComponent<CameraController>();
+        cameraController.cameraList.Insert(cameraController.cameraList.Count, transform.Find("VirtualCamera").gameObject);
+    }
+}
