954008dabcfa3d6bd1719a2e5e3022d1f013400a
feedmycat <konstantangelou@gmail.com>
2020-01-09 19:36:23 +0200

    Removed code from game manager

--- Unity/Assets/Scripts/GameManager.cs
+++ Unity/Assets/Scripts/GameManager.cs
@@ -31,31 +31,9 @@ public class GameManager : MonoBehaviour
     
     private Game _game;
 
-    public enum Turn
-    {
-        PLAYER1, PLAYER2, PLAYER3, PLAYER4
-    }
-    public Turn currentPlayerTurn = Turn.PLAYER1;
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            NextTurn();
-        }
-    }
-
     public void GoToGame(Game game)
     {
         _game = game;
         SceneManager.LoadScene("Game", LoadSceneMode.Single);
-    }
-    
-    public void NextTurn()
-    {
-        var length = System.Enum.GetNames(typeof(Turn)).Length;
-        currentPlayerTurn = (Turn)(
-            ((int)currentPlayerTurn + 1) % length
-        );
     }
 }
