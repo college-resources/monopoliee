fb9b27db324a3e00d7f3e5db0d02920ae6f6f616
feedmycat <konstantangelou@gmail.com>
2020-01-10 14:19:40 +0200

    Fixed camera controller issues

--- Unity/Assets/Scripts/Game/CameraController.cs
+++ Unity/Assets/Scripts/Game/CameraController.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public List<GameObject> cameraList;
+    public List<GameObject> playerList;
     private int currentCamera;
     void Start () {
+        playerList = GameObject.FindGameObjectsWithTag("Player").ToList();
+        playerList.Reverse();
+
+        foreach (GameObject player in playerList)
+        {
+            cameraList.Add(player.transform.Find("VirtualCamera").gameObject);
+        }
+        
         currentCamera = 0;
         for (int i = 0; i < cameraList.Count; i++){
             cameraList[i].gameObject.SetActive(false);
