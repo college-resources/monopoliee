c0135ead20cdd5063a75a146e6d81c8781a13ecc
feedmycat <konstantangelou@gmail.com>
2020-01-10 17:54:57 +0200

    Added dice sides textures and dice prefab and script

---
+++ Unity/Assets/Scripts/Game/Dice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Dice : MonoBehaviour {
+    private Sprite[] _diceSides;
+    private Image _image;
+
+    public int desiredRoll;
+
+    private void Start () {
+        _image = GetComponent<Image>();
+
+        _diceSides = Resources.LoadAll<Sprite>("DiceSides/");
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine("RollTheDice");
+        }
+    }
+    
+    private IEnumerator RollTheDice()
+    {
+        for (int i = 0; i <= 20; i++)
+        {
+            var randomDiceSide = Random.Range(0, 5);
+            _image.sprite = _diceSides[randomDiceSide];
+
+            yield return new WaitForSeconds(0.05f);
+        }
+        
+        _image.sprite = _diceSides[desiredRoll - 1];
+    }
+}
