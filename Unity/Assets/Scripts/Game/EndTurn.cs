be9861d3b7d3a39e9a61a07a6c1054936a395880
CrazyStevenz <spapanatsios@gmail.com>
2020-01-14 02:24:58 +0200

    Added EndTurn script

---
+++ Unity/Assets/Scripts/Game/EndTurn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EndTurn : MonoBehaviour
+{
+    public void OnClick()
+    {
+        APIWrapper.Instance.PlayerEndTurn((response, error) =>
+        {
+            if (error != null)
+            {
+                Debug.Log(error); // TODO: Show error to player
+            }
+        });
+    }
+}
