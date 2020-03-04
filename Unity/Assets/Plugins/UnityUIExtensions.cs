3bf9104ee063e7baf4ed32d9eea721848572aa04
CrazyStevenz <spapanatsios@gmail.com>
2020-03-04 02:16:23 +0200

    Add TextMeshProUGUI SubscribeToText extension

---
+++ Unity/Assets/Plugins/UnityUIExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TMPro;
+
+namespace UniRx
+{
+    public static class UnityUIExtensions
+    {
+        public static IDisposable SubscribeToText(this IObservable<string> source, TextMeshProUGUI text)
+        {
+            return source.SubscribeWithState(text, (x, t) => t.text = x);
+        }
+        
+        public static IDisposable SubscribeToText<T>(this IObservable<T> source, TextMeshProUGUI text)
+        {
+            return source.SubscribeWithState(text, (x, t) => t.text = x.ToString());
+        } 
+    }
+}
