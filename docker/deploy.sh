e4cb572e28ee7b0503f0be3970ad1092efedff77
jim3692 <jim3692@gmail.com>
2022-05-03 19:54:14 +0300

    Fix errors in deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 UNITY_IMAGE="gableroux/unity3d:2019.3.7f1-webgl"
 
 docker pull $UNITY_IMAGE
@@ -10,6 +12,7 @@ docker run -it --rm \
   $UNITY_IMAGE \
   bash /root/project/docker/build.sh
 
+rm -rf ../Backend/unity-build
 mv ../Unity/Build ../Backend/unity-build
 
 docker build -t cores/monopoliee -f ./Dockerfile ../Backend
